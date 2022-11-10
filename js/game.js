185c862f371eb3e2997b3c16c07df877220b66d6
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-11-10 14:03:37 -0500

    init

--- js/game.js
+++ js/game.js
@@ -11,8 +11,19 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
   //set background color
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // Center placement
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 const game = new Phaser.Game(config);
