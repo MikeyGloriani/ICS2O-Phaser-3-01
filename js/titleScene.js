30de6cf1aa1acf73eb2963a2a77dba6f0834453f
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-11-23 19:07:44 +0000

    Prettified Code!

--- js/titleScene.js
+++ js/titleScene.js
@@ -8,9 +8,13 @@ class TitleScene extends Phaser.Scene {
   constructor() {
     super({ key: "titleScene" });
 
-    this.titleSceneBackgroundImage = null
-    this.titleSceneText = null
-    this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center'}
+    this.titleSceneBackgroundImage = null;
+    this.titleSceneText = null;
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    };
   }
 
   init(data) {
@@ -22,17 +26,20 @@ class TitleScene extends Phaser.Scene {
     this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg");
   }
 
-  create(data) {    
-    this.titleSceneBackgroundImage = this.add.sprite(
-    0,
-    0,
-    "titleSceneBackground"
-  ).setScale(2.75)
-  this.titleSceneBackgroundImage.x = 1920 / 2
-  this.titleSceneBackgroundImage.y = 1080 / 2
+  create(data) {
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(2.75);
+    this.titleSceneBackgroundImage.x = 1920 / 2;
+    this.titleSceneBackgroundImage.y = 1080 / 2;
 
-  this.titleSceneText = this.add.text(1920 / 2, (1080/2) + 350, 'Space Aliens', this.titleSceneTextStyle)
-}
+    this.titleSceneText = this.add.text(
+      1920 / 2,
+      1080 / 2 + 350,
+      "Space Aliens",
+      this.titleSceneTextStyle
+    );
+  }
 
   update(time, delta) {}
 }
