de3f60b97629d389fa4fe89e38797c624ac60764
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-11-23 14:05:54 -0500

    init

--- js/titleScene.js
+++ js/titleScene.js
@@ -8,7 +8,9 @@ class TitleScene extends Phaser.Scene {
   constructor() {
     super({ key: "titleScene" });
 
-    this.titleSceneBackgroundImage = null;
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center'}
   }
 
   init(data) {
@@ -20,13 +22,17 @@ class TitleScene extends Phaser.Scene {
     this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg");
   }
 
-  create(data) {
-    this.titleSceneBackgroundImage = this.add
-      .sprite(0, 0, "titleSceneBackground")
-      .setScale(2.75);
-    this.titleSceneBackgroundImage.x = 1920 / 2;
-    this.titleSceneBackgroundImage.y = 1080 / 2;
-  }
+  create(data) {    
+    this.titleSceneBackgroundImage = this.add.sprite(
+    0,
+    0,
+    "titleSceneBackground"
+  ).setScale(2.75)
+  this.titleSceneBackgroundImage.x = 1920 / 2
+  this.titleSceneBackgroundImage.y = 1080 / 2
+
+  this.titleSceneText = this.add.text(1920 / 2, (1080/2) + 350, 'Space Aliens', this.titleSceneTextStyle)
+}
 
   update(time, delta) {}
 }
