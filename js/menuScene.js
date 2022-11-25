0b3b2dc4d80de07ff310ac00d606889a92b9e8f6
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-11-25 19:23:31 +0000

    Prettified Code!

--- js/menuScene.js
+++ js/menuScene.js
@@ -33,15 +33,15 @@ class MenuScene extends Phaser.Scene {
     this.menuSceneBackgroundImage.x = 1920 / 2;
     this.menuSceneBackgroundImage.y = 1080 / 2;
 
-  this.startButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, "startButton")
-  this.startButton.setInteractive({ useHandCursor: true})
-  this.startButton.on('pointerdown', () => this.clickbutton())
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton");
+    this.startButton.setInteractive({ useHandCursor: true });
+    this.startButton.on("pointerdown", () => this.clickbutton());
   }
 
   update(time, delta) {}
 
-  clickbutton () {
-    this.scene.start('gamescene')
+  clickbutton() {
+    this.scene.start("gamescene");
   }
 }
 
