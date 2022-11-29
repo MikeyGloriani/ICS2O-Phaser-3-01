ab8146f3f637351f92e2679744755d92b255b1a6
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-11-29 19:48:22 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -6,10 +6,10 @@
 
 class GameScene extends Phaser.Scene {
   constructor() {
-    super({ key: "gameScene" })
+    super({ key: "gameScene" });
 
-    this.background = null
-    this.ship = null
+    this.background = null;
+    this.ship = null;
   }
 
   init(data) {
@@ -17,18 +17,18 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    console.log("Game Scene")
+    console.log("Game Scene");
 
     //images
-    this.load.image('starBackground', 'assets/starBackground.png')
-    this.load.image('ship', 'assets/spaceShip.png')
+    this.load.image("starBackground", "assets/starBackground.png");
+    this.load.image("ship", "assets/spaceShip.png");
   }
 
   create(data) {
-    this.background = this.add.image(0,0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0,0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920/2, 1080-100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
   }
 
   update(time, delta) {}
