d1f7debbd9acda98b012b9161547825bbf474998
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-12-05 14:38:43 -0500

    init

--- js/gameScene.js
+++ js/gameScene.js
@@ -31,7 +31,20 @@ class GameScene extends Phaser.Scene {
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    // called 60 times a second, hopefully.
+
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+
+    if(keyLeftObj.isDown === true) {
+      this.ship.x = this.ship.x - 15
+    }
+
+    if(keyRightObj.isDown === true) {
+      this.ship.x = this.ship.x + 15
+    }
+  }
 }
 
 export default GameScene;
