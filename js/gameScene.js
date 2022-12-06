65e5ac00abf5cb733c9c8b28a43142d5694ca685
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-12-06 14:40:17 -0500

    init

--- js/gameScene.js
+++ js/gameScene.js
@@ -20,15 +20,17 @@ class GameScene extends Phaser.Scene {
     console.log("Game Scene");
 
     //images
-    this.load.image("starBackground", "assets/starBackground.png");
-    this.load.image("ship", "assets/spaceShip.png");
+    this.load.image("starBackground", "assets/starBackground.png")
+    this.load.image("ship", "assets/spaceShip.png")
+    this.load.image("missile", "assets/missile.png")
   }
 
   create(data) {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
     this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+    this.missileGroup = this.physics.add.group()
   }
 
   update(time, delta) {
@@ -38,17 +40,18 @@ class GameScene extends Phaser.Scene {
     const keyRightObj = this.input.keyboard.addKey("RIGHT");
 
     if (keyLeftObj.isDown === true) {
-      this.ship.x = this.ship.x -= 15;
+      this.ship.x = this.ship.x - 15
       if (this.ship.x < 0) {
         this.ship.x = 0;
       }
     }
-
     if (keyRightObj.isDown === true) {
-      this.ship.x = this.ship.x += 15;
-      if (this.ship.x > 1920) {
-        this.ship.x = 1920;
+      this.ship.x = this.ship.x + 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
       }
     }
   }
 }
+
+export default GameScene
