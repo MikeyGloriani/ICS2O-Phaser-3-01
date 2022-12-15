b4c8dda9b3cbd48b6dead921a0aa2139c7ea04ac
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-12-15 14:19:24 -0500

    init

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,8 +10,8 @@ class GameScene extends Phaser.Scene {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1; // gets a number between 1 and 1920
     let alienXVelocity = Math.floor(Math.random() * 50) + 1; // gets a number between 1 and 50
     alienXVelocity *= Math.round(Math.random()) ? 1 : -1;
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien");
-    anAlien.body.velocity.y = 600;
+    const anAlien = this.physics.add.sprite(alienXLocation, 100, "alien");
+    anAlien.body.velocity.y = 200;
     anAlien.body.velocity.y = alienXVelocity;
     this.alienGroup.add(anAlien);
   }
